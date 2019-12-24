6e4ca5f47d1bbae8214bbb5e017bfc3074b41cf8
Nemanja Stijak <nemanja.stijak@redbox.com>
2019-12-24 12:32:38 +0100

    Grid fragment

---
+++ app/src/main/java/com/interventure/hackathon/numbers/NumbersGridFragment.kt
@@ -0,0 +1,43 @@
+package com.interventure.hackathon.numbers
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.iterator
+
+import kotlinx.android.synthetic.main.grid_fragment.*
+
+class NumbersGridFragment : Fragment(), View.OnClickListener {
+
+    companion object {
+        fun newInstance() =
+            NumbersGridFragment()
+    }
+
+    private lateinit var viewModel: NumbersGridViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var v = inflater.inflate(R.layout.grid_fragment, container, false)
+        for (view in choice_grid.iterator()) {
+            view.setOnClickListener(this)
+        }
+        return inflater.inflate(R.layout.grid_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(NumbersGridViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+
+}
---
+++ app/src/main/java/com/interventure/hackathon/numbers/NumbersGridViewModel.kt
@@ -0,0 +1,7 @@
+package com.interventure.hackathon.numbers
+
+import androidx.lifecycle.ViewModel
+
+class NumbersGridViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
