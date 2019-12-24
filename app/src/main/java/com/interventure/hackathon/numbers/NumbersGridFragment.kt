7846f7beffddf07a2a868742532eb7beabd21d41
Dejan Mitrovic <dejan.mitrovic@redbox.com>
2019-12-24 13:36:05 +0100

    iterator

--- app/src/main/java/com/interventure/hackathon/numbers/NumbersGridFragment.kt
+++ app/src/main/java/com/interventure/hackathon/numbers/NumbersGridFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.iterator
 
 import kotlinx.android.synthetic.main.grid_fragment.*
 
